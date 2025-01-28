395891a624506a37b40e3ff957a81c3d3fa246b6
rosa <rosahadi19@gmail.com>
2025-01-28 17:48:28 +0300

    Fix Slither warnings and improve contract security
    - Replaced `abi.encodePacked` with `string.concat` in `uri` function for improved readability and performance.
    - Added check to revert if `forgeContract` address is the zero address in `setForgeContract` function to prevent setting an invalid address.
    - Applied `nonReentrant` modifier to `trade` function to prevent potential reentrancy vulnerabilities.
    
    These changes address warnings and enhance security.

--- hardhat/contracts/TokenCollection.sol
+++ hardhat/contracts/TokenCollection.sol
@@ -5,8 +5,9 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract TokenCollection is ERC1155, Ownable, ERC1155Burnable {
+contract TokenCollection is ERC1155, Ownable, ERC1155Burnable, ReentrancyGuard {
     using Strings for uint256;
 
     string public name;
@@ -26,6 +27,7 @@ contract TokenCollection is ERC1155, Ownable, ERC1155Burnable {
     error InvalidTradeTokens();
     error InsufficientBalance();
     error UnauthorizedForge();
+    error InvalidAddress();
 
     constructor()
         ERC1155(
@@ -52,12 +54,10 @@ contract TokenCollection is ERC1155, Ownable, ERC1155Burnable {
         if (totalSupply[tokenId] == 0) revert TokenNotMinted();
 
         return
-            string(
-                abi.encodePacked(
-                    super.uri(tokenId),
-                    Strings.toString(tokenId),
-                    ".json"
-                )
+            string.concat(
+                super.uri(tokenId),
+                Strings.toString(tokenId),
+                ".json"
             );
     }
 
@@ -85,17 +85,21 @@ contract TokenCollection is ERC1155, Ownable, ERC1155Burnable {
         uint256 tokenToGive,
         uint256 tokenToReceive,
         uint256 amount
-    ) public {
+    ) public nonReentrant {
         if (tokenToGive > 2 || tokenToReceive > 2) revert InvalidTradeTokens();
         if (balanceOf(msg.sender, tokenToGive) < amount) {
             revert InsufficientBalance();
         }
+
+        totalSupply[tokenToGive] -= amount;
+        totalSupply[tokenToReceive] += amount;
 
         _burn(msg.sender, tokenToGive, amount);
         _mint(msg.sender, tokenToReceive, amount, "");
     }
 
     function setForgeContract(address _forgeContract) external onlyOwner {
+        if (_forgeContract == address(0)) revert InvalidAddress();
         forgeContract = _forgeContract;
     }
 }

a63d009605dd1aa47b0c618cc2e955f8fbc61abb
rosa <rosahadi19@gmail.com>
2025-01-31 12:30:29 +0300

    Add Rajdhani font to RootLayout and apply globally

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -1,22 +1,23 @@
 import { headers } from "next/headers";
 import ContextProvider from "@/context";
+import { Rajdhani } from "next/font/google";
 import "./globals.css";
+
+const rajdhani = Rajdhani({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+});
 
 export default async function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   const headersData = await headers();
   const cookies = headersData.get("cookie");
 
   return (
-    <html lang="en">
-      <body
-        className={
-          "max-w-[120rem] w-[90%] mx-auto bg-background h-[100rem]"
-        }
-      >
+    <html lang="en" className={rajdhani.className}>
+      <body className="max-w-[120rem] w-[90%] mx-auto bg-background h-[100rem]">
         <ContextProvider cookies={cookies}>
           {children}
         </ContextProvider>
